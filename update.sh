2e4d172ce963dfd853bfce3090cb94797fca1958
SaulDoesCode <saul@kurshok.space>
2023-08-08 06:01:05 +0200

    update backend too

--- update.sh
+++ update.sh
@@ -3,3 +3,8 @@ curl -JLO https://raw.githack.com/SaulDoesCode/shok/master/static/space.html
 curl -JLO https://raw.githack.com/SaulDoesCode/shok/master/static/ks.css
 curl -JLO https://raw.githack.com/SaulDoesCode/shok/master/static/domlib.js
 curl -JLO https://raw.githack.com/SaulDoesCode/shok/master/static/icon.svg
+cd ../src/
+curl -JLO https://raw.githack.com/SaulDoesCode/shok/master/src/main.rs
+cd ../
+curl -JLO https://raw.githack.com/SaulDoesCode/shok/master/Cargo.toml
+nohup cargo build --release &
